3d3e10463ac9c39ad0d32e59b1ebf5ef6bb18e61
muratolgac <muratolgac@yandex.com>
2021-11-25 14:54:50 +0300

    return

---
+++ function-examples.js
@@ -0,0 +1,41 @@
+function printNumbersToEleven() {
+    for (var i = 1; i < 11; i++) {
+        console.log(i);
+    }
+}
+
+function printHello() {
+    console.log("hello");
+}
+
+function printHelloWithName(name) {
+    console.log("hello " + name);
+}
+
+function getFullName(name, surname) {
+    return name + " " + surname;
+}
+
+function getMultiplicationByRandom(num) {
+    // random sayısını tam sayıya çevirme
+    const random = Math.random() * 9 + 1;
+    console.log("Random:", random);
+    return num * random;
+}
+
+const name = "Toruk2";
+const fullname = getFullName(name, "Macto");
+//console.log(fullname);
+//printHelloWithName(name);
+
+const result1 = getMultiplicationByRandom(5);
+console.log("result1:", result1);
+
+const result2 = getMultiplicationByRandom(4);
+console.log("result2:", result2);
+
+if (result1 > result2) {
+    console.log(result1);
+} else {
+    console.log(result2);
+}
