bc8d0203497e18d0e3727a4a90a36824b6609b95
muratolgac <muratolgac@yandex.com>
2021-11-27 12:20:54 +0300

    isOddv2

--- isOdd.js
+++ isOdd.js
@@ -1,4 +1,10 @@
 function isOdd(num) {
-    return num % 2 === 1;
+    return num % 2 == 1;
 }
 console.log(isOdd(3));
+
+for (let a=0; a<=100; a++) {
+    if(a % 2 == 0){
+        console.log(a);
+    }
+}
