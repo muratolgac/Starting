fdea7b20fd3cb3f700ecfedcde512cabb50af264
muratolgac <muratolgac@yandex.com>
2021-11-27 12:12:15 +0300

    string literal examples

---
+++ string-examples.js
@@ -0,0 +1,14 @@
+//const my = { name: 'Murat', surname: 'Olgac', age: 28 }
+//const bilgi = 'My name is ' + my.name + ' ' + my.surname + " and age is " + my.age;
+//console.log(bilgi);
+
+
+//const my={name:'Murat',surname:'Olgac',age:28}
+//const bilgi='My name is '+my.name+' '+my.surname+" \t age is "+my.age;
+//console.log(bilgi)
+
+const a = 3
+const b = 5
+console.log (`sum of a and b is ${a+b}`);
+const my={name:'Murat',surname:'Olgac',age:28}
+console.log (`My name is ${my.name} ${my.surname}`);
