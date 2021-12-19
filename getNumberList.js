a7848e255ca70069655ff32deb31b637a9d73693
muratolgac <muratolgac@yandex.com>
2021-12-19 13:13:24 +0300

    updates

--- getNumberList.js
+++ getNumberList.js
@@ -1,21 +1,21 @@
 const getNumberList = (maxNumber) => {
-    const numberList = [];
-    for (let i = 1; i <= maxNumber; i++) {
-      numberList.push(i);
-    }
-    return numberList;
-  };
+  const numberList = [];
+  for (let i = 1; i <= maxNumber; i++) {
+    numberList.push(i);
+  }
+  return numberList;
+};
 const numberList = getNumberList(20);
-const filteredNumberList = numberList.filter((num) => num<14);
-const isPrime = filteredNumberList.filter ((num)=>{
-    for (let i = 2; i < num; i++) {
-        if (num % i == 0) {
-          return false;
-        }
-      }
-      return num > 1;
-})
-const mappedNumberList = isPrime.map((num,index)=>{
-        return `Kümenin '${index+1}'. elemanı ${num}`
-})
+const filteredNumberList = numberList.filter((num) => num < 14);
+const isPrime = filteredNumberList.filter((num) => {
+  for (let i = 2; i < num; i++) {
+    if (num % i == 0) {
+      return false;
+    }
+  }
+  return num > 1;
+});
+const mappedNumberList = isPrime.map((num, index) => {
+  return `Kümenin '${index + 1}'. elemanı ${num}`;
+});
 console.log(mappedNumberList);
